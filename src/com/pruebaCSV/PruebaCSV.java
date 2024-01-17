37cbf4f63bc523fe6ae77d20cac229804e66692c
joansnb <joansnb@gmail.com>
2024-01-17 14:30:20 +0100

    terminado el lector CSV

--- src/com/pruebaCSV/PruebaCSV.java
+++ src/com/pruebaCSV/PruebaCSV.java
@@ -3,6 +3,8 @@ package com.pruebaCSV;
 //Ejemplo de pokemon
 public class PruebaCSV {
     public static void main(String[] args){
+        LectorCSV miLector = new LectorCSV();
 
+        miLector.leeCSV("./datos/pokemon.csv");
     }
 }
